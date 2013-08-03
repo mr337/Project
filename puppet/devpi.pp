073adcdf581f1b31f9a8b8821dedece6790c5209
Lee Hicks <mr337@mr337.com>
2013-08-02 21:38:13 -0500

    Baseline complete
    CF works and added availability zone
    Puppet brings up a basic devpi server

--- puppet/devpi.pp
+++ puppet/devpi.pp
@@ -3,6 +3,7 @@
 #Package stuff
 package{'nginx':
     ensure => present,
+    before => File['/etc/nginx/conf.d/server.conf']
 }
 
 package{'python-pip':
@@ -11,24 +12,28 @@ package{'python-pip':
 
 #create symlink to fix pip provider issue in python2.6
 file{'/usr/bin/pip':
-    ensure => 'link',
-    target => '/usr/bin/pip-python'
+    ensure  => 'link',
+    target  => '/usr/bin/pip-python',
+    require => Package['python-pip']
 }
 
 package{'devpi':
-    ensure => present,
-    provider => pip
+    ensure   => present,
+    provider => pip,
+    require  => File['/usr/bin/pip']
 }
 
 
 #Prep and install devpi server
 file{'/devpi':
-    ensure => 'directory'
+    ensure  => 'directory',
+    require => Package['devpi']
 }
 
 exec{'deployDevpi':
     command => 'devpi-server --gendeploy=/devpi',
-    path => '/'
+    path    => '/usr/bin/',
+    require => File['/devpi'] 
 }
 
 #copy devpi nginx conf
@@ -36,10 +41,24 @@ file{'/etc/nginx/conf.d/server.conf':
     owner => root,
     group => root,
     mode => 644,
-    source => '/devpi/etc/nginx-devpi.conf'
+    source => '/devpi/etc/nginx-devpi.conf',
+    require => Exec['deployDevpi']
 }
 
-service{'ngingx':
-    ensure => 'running',
-    enable => 'true'
+#restart service when site added to conf.d
+service{'nginx':
+    ensure    => 'running',
+    enable    => 'true',
+    subscribe => File['/etc/nginx/conf.d/server.conf']
+
 }
+
+#start devpi server itself
+exec{'runDevpi':
+    command   => 'devpi-ctl start all',
+    path      => '/devpi/bin/',
+    subscribe => Exec['deployDevpi']
+}
+
+#TODO
+#Create cron to kick the puppet
